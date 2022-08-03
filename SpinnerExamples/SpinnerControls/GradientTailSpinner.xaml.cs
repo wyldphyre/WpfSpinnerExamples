42c057decbf20ee46e4eeeae0441d1352250cbc0
Craig Reynolds <craig.reynolds@health.telstra.com>
2022-08-03 08:25:40 +0800

    Initial commit with examples

---
+++ SpinnerExamples/SpinnerControls/GradientTailSpinner.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SpinnerExamples.SpinnerControls
+{
+    /// <summary>
+    /// Interaction logic for SpinnerGradientTail.xaml
+    /// </summary>
+    public partial class GradientTailSpinner : UserControl
+    {
+        public GradientTailSpinner()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SpinnerExamples/SpinnerConverters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace SpinnerExamples
+{
+    /// <summary>
+    /// This converter is used to produce a clipping rectangle for the "ProgressArc" ellipse.
+    /// It's job is to a rectangle in the top right of the width and height it is given, which will
+    /// be used to clip the ellipse.
+    /// </summary>
+    public class SpinnerClipRectangleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var halfWidth = (double)values[0] / 2;
+            var halfHeight = (double)values[1] / 2;
+
+            return new System.Windows.Rect(halfWidth, 0, halfWidth, halfHeight);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
